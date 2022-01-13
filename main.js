01201f76f280c97b7938aee37fea85e45f18676b
Magda Konopka <madzia199652@interia.pl>
2022-01-13 15:38:24 +0100

    new look for articles

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-const firstName = 'Magda';
-const age = '25';
-
+const firstName = "Magda";
+const age = "25";
 
 console.log(firstName);
 console.log(age);
 console.log(`Cześć!Nazywam się ${firstName} i mam ${age}lat.`);
+const heading = document.querySelector('.hotpink-header--js');
+heading.innerHTML =`Cześć!Nazywam się ${firstName} i mam ${age}lat.` 
+
+
