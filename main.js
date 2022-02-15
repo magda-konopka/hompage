d2734a465c61a6c19797241da3c8e029ef580a6e
Magda Konopka <madzia199652@interia.pl>
2022-02-15 12:36:26 +0100

    ad hamburger

--- main.js
+++ main.js
@@ -7,4 +7,19 @@ console.log(`Cześć!Nazywam się ${firstName} i mam ${age}lat.`);
 const heading = document.querySelector('.hotpink-header--js');
 heading.innerHTML =`Cześć!Nazywam się ${firstName} i mam ${age}lat.` 
 
+greet(age, firstName);
+
+function greet(age,firstName) {
+    console.log(
+        `Cześć!Nazywam się ${firstName} i mam ${age}lat.`
+
+    );
+}
+
+const hamburger = document.querySelector('.hamburger--js');
+hamburger.addEventListener('click', () => {
+    const nav = document.querySelector('.navigation--js');
+    nav.classList.toggle('navigation--open');
+})
+
 
